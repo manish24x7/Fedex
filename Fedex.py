b0d2308a91877815b8e1ba7eb0eb47ea5a41b524
manish24x7 <manishshaw323@gmail.com>
2017-08-13 01:27:41 +0530

    Finally Solved

--- Fedex.py
+++ Fedex.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-tracking_number = input();
+tracking_number = '744668909687'
 
 data = requests.post('https://www.fedex.com/trackingCal/track', data={
     'data': json.dumps({
@@ -28,3 +28,26 @@ data = requests.post('https://www.fedex.com/trackingCal/track', data={
     'format': 'json',
     'version': 99
 }).json()
+
+tracking_number_key = 'trackingNbr'
+ship_time_key = 'displayShipDt'
+status_key = 'status'
+ship_arrival_key = 'displayActDeliveryDateTime'
+output = {}
+for key, value in data.items():
+    narrow = value
+    #narrow more into packageList list
+    for key, value in narrow.items():
+        if key == 'packageList':
+            narrow = value
+    for x, y in narrow[0].items():
+        if x == tracking_number_key:
+            output['tracking no'] = y
+        elif x == ship_time_key:
+            output['ship date'] = y
+        elif x == status_key:
+            output['status'] = y
+        elif x == ship_arrival_key:
+            output['scheduled delivery'] = y
+
+print(output)
